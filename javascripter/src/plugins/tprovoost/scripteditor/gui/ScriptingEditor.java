cc31882221faf5cc94b65f962c831090ba4bd5f8
Timothée Lecomte <lecomte.timothee@gmail.com>
2013-12-11 15:23:20 +0100

    Store the preferences in the fully-qualified plugin name
    This is more in-line with the spirit of these identifiers

--- javascripter/src/plugins/tprovoost/scripteditor/gui/ScriptingEditor.java
+++ javascripter/src/plugins/tprovoost/scripteditor/gui/ScriptingEditor.java
@@ -97,7 +97,7 @@ public class ScriptingEditor extends IcyFrame implements ActionListener
 	private static final int ctrlMask = SystemUtil.getMenuCtrlMask();
 	private static final int MAX_RECENT_FILES = 20;
 	private static final String STRING_LAST_DIRECTORY = "lastDirectory";
-	private XMLPreferences prefs = PluginsPreferences.getPreferences().node("scripteditor");
+	private XMLPreferences prefs = PluginsPreferences.getPreferences().node("plugins.tprovoost.scripteditor.main.ScriptEditorPlugin");
 
 	private static final boolean IS_PYTHON_INSTALLED = ScriptEngineHandler.factory.getEngineByExtension("py") != null;
 	private static final String PREF_IDX = "idxTab";
