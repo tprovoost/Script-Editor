tprovoost/Script-Editor

commit ed04e9a593c04fe7a476394c4e9885a1ef8b4039

(ScriptingEditor) Fix the saveState call when closing a single tab

javascripter/src/plugins/tprovoost/scripteditor/gui/ButtonTabComponent.java
@@ -168,6 +168,8 @@ public class ButtonTabComponent extends JPanel
 
     /**
      * Remove this pane from the {@link JTabbedPane}.
+     * 
+     * @return false if the user has cancelled the operation
      */
     public boolean deletePane()
     {
@@ -190,9 +192,7 @@ public class ButtonTabComponent extends JPanel
 	        }
 	        return true;
         }
-        else
-        {
-        	return false;
-        }
+        
+        return ok;
     }
 }

javascripter/src/plugins/tprovoost/scripteditor/gui/ScriptingEditor.java
@@ -992,9 +992,10 @@ public class ScriptingEditor extends IcyFrame implements ActionListener
 		Component c = tabbedPane.getTabComponentAt(i);
 		if (c instanceof ButtonTabComponent)
 		{
-			return ((ButtonTabComponent) c).deletePane();
+			boolean res = ((ButtonTabComponent) c).deletePane();
+			saveState();
+			return res;
 		}
-		saveState();
 		return true;
 	}
 

commit df43468fab010a7a3265d0decf46a379d8031bc9

(ButtonTabComponent) Fix the tab name, broken by commit ed04e9a593

javascripter/src/plugins/tprovoost/scripteditor/gui/ButtonTabComponent.java
@@ -55,7 +55,13 @@ public class ButtonTabComponent extends JPanel
 
             public String getText()
             {
-                return getPanel().getPanelName();
+                JTabbedPane pane = editor.getTabbedPane();
+                int i = pane.indexOfTabComponent(ButtonTabComponent.this);
+                if (i != -1)
+                {
+                    return pane.getTitleAt(i);
+                }
+                return "";
             }
         };
 